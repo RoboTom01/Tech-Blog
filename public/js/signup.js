c5e4eb54919c5458d51bfc249397ad28d324244d
Thomas <tmrtw8@gmail.com>
2023-05-20 02:05:47 -0600

    fixedSignUp

---
+++ public/js/signup.js
@@ -0,0 +1,26 @@
+const signupFormHandler = async (event) => {
+    console.log("signing up")
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users/signup', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to signup');
+      }
+    }
+};
+
+
+document
+.getElementById("signup-form")
+.addEventListener('submit', signupFormHandler);
