30c25aadb5d1b58543f9a598ef068193f8531d63
Thomas <tmrtw8@gmail.com>
2023-05-19 21:52:20 -0600

    nodemon

--- models/index.js
+++ models/index.js
@@ -2,13 +2,25 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-User.hasMany(Post, {
+// User.hasMany(Post, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
+
+Post.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
 
 module.exports = { User, Post, Comment }
